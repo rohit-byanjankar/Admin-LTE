7483f0c8897739dc45bda16db6eb48a3ea27961a
rohit-byanjankar <rohitbenz09@gmail.com>
2019-06-03 10:00:59 +0545

    Template Setup Properly with Overrides

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,16 +4,15 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card" style="margin: 250px 400px;">
+            <div class="card mt-3">
                 <div class="card-header">Dashboard</div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-                    You are logged in!
+
                 </div>
             </div>
         </div>
