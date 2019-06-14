0b10dd4e2547847e8da76f1b281366491765da23
cas555 <oasisnoel01@gmail.com>
2019-06-14 15:26:33 +0545

    logincontroller changes

--- routes/web.php
+++ routes/web.php
@@ -11,33 +11,35 @@
 |
 */
 
-//Route::get('/', function () {
+// Route::get('/', function () {
 //    return view('auth.login');
-//});
-//
+// });
+   
 
 Route::middleware('auth')->group(function(){
     Route::get('/home', 'HomeController@index')->name('home');
+});
 
 //    Route::resource('categories', 'CategoriesController');
-//    Route::resource('tags', 'TagsController')
+//    Route::resource('tags', 'TagsController');
 //    Route::resource('posts', 'PostsController');
 //    Route::resource('roles', 'RolesController');
 //    Route::get('rolePermission','PermissionController@selectRole');
 //    Route::get('rolePermission/{role}','PermissionController@getPermission');
 //    Route::post('role-permission-save', 'PermissionController@checkPermissionPost')->name('permission-post');
-//
+//    Route::resource('events', 'EventsController');
 //    Route::get('trashed-posts', 'PostsController@trashed')->name('trashed-posts.index');
 //    Route::put('restore-post/{post}', 'PostsController@restore')->name('restore-posts');
+  
 
-});
+// });
 
-//Route::middleware(['auth','admin'])->group(function(){
+// Route::middleware(['auth','admin'])->group(function(){
 //    Route::get('users', 'UsersController@index')->name('users.index');
 //    Route::get('users/profile', 'UsersController@edit')->name('users.edit-profile');
 //    Route::post('users/{user}/make-admin', 'UsersController@makeAdmin')->name('users.make-admin');
 //    Route::put('users/profile', 'UsersController@update')->name('users.update-profile');
-//});
+// });
 
 //REGISTER
 Auth::routes();
