cbf48c5e2eeed7aecc9fc4f6fb4d22c004b4a9d3
cas555 <oasisnoel01@gmail.com>
2019-06-24 19:49:58 +0545

    old theme is back

--- Modules/Home/Resources/views/index.blade.php
+++ Modules/Home/Resources/views/index.blade.php
@@ -7,8 +7,8 @@
 			<div class="container">
 				<div class="main-section-data">
 					<div class="row">
-						<div class="col-lg-3 col-md-4 ">
-							<div class="main-left-sidebar ">
+						<div class="col-lg-3 col-md-4 pd-left-none no-pd ">
+							<div class="main-left-sidebar  no-margin ">
 								<div class="user-data full-width">
 									<div class="user-profile">
 										<div class="username-dt">
@@ -50,14 +50,22 @@
 						</div>
 
 
-						<div class="col-lg-7">
+						<div class="col-lg-6 col-md-8 no-pd">
 							<div class="main-ws-sec ">
-								<div class="post-st">
-									<ul>
-
+								<div class="post-topbar">
+									<div class="user-picy">
+										<img src="http://via.placeholder.com/100x100" alt="">
+									</div>
+									<div class="post-st">
+										<ul>
+											
 										<li><a class="" href="{{ route('userposts.create')}}" title="">Add a Post</a></li>
-									</ul>
+										</ul>
+									</div>
+									<!--post-st end-->
 								</div>
+								<!--post-topbar end-->
+								
 								<!--post-st end-->
 
 
@@ -77,16 +85,12 @@
 										</div>
 
 										<div class="job_descp">
-											<h3>{{$post->description}}</h3>
-											<ul class="job-dt">
-												<li>
-													<a href="#" title="">
-														Full Time
-													</a>
-												</li>
-											</ul>
+												<a href="{{route('userposts.show',$post->id)}}">
+												<h3>{{$post->title}}</h3>
+												</a>
+											
 
-											<p>{{$post->content}} <a href="#" title="">view more</a></p>
+											<p>{{$post->description}} </p>
 
 											<ul class="skill-tags">
 												<i class="fa fa-tag"></i>
@@ -108,10 +112,10 @@
 							<!--main-ws-sec end-->
 						</div>
 
-						<div class="col-lg-2">
+						<div class="col-lg-3 pd-right-none no-pd">
 							<div class="right-sidebar">
 								<div class="widget widget-about">
-									
+
 									<h3>Community Media</h3>
 									<span>Connect with your Community</span>
 									<div class="sign_link">
@@ -128,117 +132,6 @@
 		</div>
 	</main>
 
-
-
-
-	<div class="post-popup pst-pj">
-		<div class="post-project">
-			<h3>Post a project</h3>
-			<div class="post-project-fields">
-				<form>
-					<div class="row">
-						<div class="col-lg-12">
-							<input type="text" name="title" placeholder="Title">
-						</div>
-						<div class="col-lg-12">
-							<div class="inp-field">
-								<select>
-									<option>Category</option>
-									<option>Category 1</option>
-									<option>Category 2</option>
-									<option>Category 3</option>
-								</select>
-							</div>
-						</div>
-						<div class="col-lg-12">
-							<input type="text" name="skills" placeholder="Skills">
-						</div>
-						<div class="col-lg-12">
-							<div class="price-sec">
-								<div class="price-br">
-									<input type="text" name="price1" placeholder="Price">
-									<i class="la la-dollar"></i>
-								</div>
-								<span>To</span>
-								<div class="price-br">
-									<input type="text" name="price1" placeholder="Price">
-									<i class="la la-dollar"></i>
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-12">
-							<textarea name="description" placeholder="Description"></textarea>
-						</div>
-						<div class="col-lg-12">
-							<ul>
-								<li><button class="active" type="submit" value="post">Post</button></li>
-								<li><a href="#" title="">Cancel</a></li>
-							</ul>
-						</div>
-					</div>
-				</form>
-			</div>
-			<!--post-project-fields end-->
-			<a href="#" title=""><i class="la la-times-circle-o"></i></a>
-		</div>
-		<!--post-project end-->
-	</div>
-	<!--post-project-popup end-->
-
-	<div class="post-popup job_post">
-		<div class="post-project">
-			<h3>Post a job</h3>
-			<div class="post-project-fields">
-				<form>
-					<div class="row">
-						<div class="col-lg-12">
-							<input type="text" name="title" placeholder="Title">
-						</div>
-						<div class="col-lg-12">
-							<div class="inp-field">
-								<select>
-									<option>Category</option>
-									<option>Category 1</option>
-									<option>Category 2</option>
-									<option>Category 3</option>
-								</select>
-							</div>
-						</div>
-						<div class="col-lg-12">
-							<input type="text" name="skills" placeholder="Skills">
-						</div>
-						<div class="col-lg-6">
-							<div class="price-br">
-								<input type="text" name="price1" placeholder="Price">
-								<i class="la la-dollar"></i>
-							</div>
-						</div>
-						<div class="col-lg-6">
-							<div class="inp-field">
-								<select>
-									<option>Full Time</option>
-									<option>Half time</option>
-								</select>
-							</div>
-						</div>
-						<div class="col-lg-12">
-							<textarea name="description" placeholder="Description"></textarea>
-						</div>
-						<div class="col-lg-12">
-							<ul>
-								<li><button class="active" type="submit" value="post">Post</button></li>
-								<li><a href="#" title="">Cancel</a></li>
-							</ul>
-						</div>
-					</div>
-				</form>
-			</div>
-			<!--post-project-fields end-->
-			<a href="#" title=""><i class="la la-times-circle-o"></i></a>
-		</div>
-		<!--post-project end-->
-	</div>
-	<!--post-project-popup end-->
 </div>
 <!--theme-layout end-->
 @endsection
